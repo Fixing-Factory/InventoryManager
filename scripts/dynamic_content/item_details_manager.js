b768bf7ff38242d50881dabb5e0a4d54701d2a19
Alex Jones <alexander.jones@softwire.com>
2024-06-14 19:31:25 +0100

    Add update button

--- scripts/dynamic_content/item_details_manager.js
+++ scripts/dynamic_content/item_details_manager.js
@@ -1,6 +1,7 @@
 export class ItemDetailsManager {
   constructor() {
     this.allDetailsElement = document.getElementById("all-details")
+    this.allFormsElement = document.getElementById("all-forms")
     this.itemDetailsElement = document.getElementById("item-details")
     this.fixingDetailsElement = document.getElementById("fixing-details")
     this.testingDetailsElement = document.getElementById("testing-details")
@@ -8,6 +9,7 @@ export class ItemDetailsManager {
     this.loggingDetailsButton = document.getElementById("logging-button")
     this.fixingDetailsButton = document.getElementById("fixing-button")
     this.testingDetailsButton = document.getElementById("testing-button")
+    this.updateButton = document.getElementById("update-button")
 
     this.setupDisplayToggles()
   }
@@ -16,10 +18,28 @@ export class ItemDetailsManager {
     this.loggingDetailsButton.addEventListener("click", () => { this.displayItemDetails() })
     this.fixingDetailsButton.addEventListener("click", () => { this.displayFixingDetails() })
     this.testingDetailsButton.addEventListener("click", () => { this.displayTestingDetails() })
+    this.updateButton.addEventListener("click", () => { this.toggleUpdate() })
+  }
+
+  toggleUpdate() {
+    if (this.updateButton.className === "active") {
+      this.updateButton.className = ""
+      this.displayAllDetails()
+    } else {
+      this.updateButton.className = "active"
+      this.displayAllForms()
+    }
+    
   }
 
   displayAllDetails() {
     this.allDetailsElement.className = "enabled"
+    this.allFormsElement.className = "disabled"
+  }
+
+  displayAllForms() {
+    this.allDetailsElement.className = "disabled"
+    this.allFormsElement.className = "enabled"
   }
 
   displayItemDetails() {
